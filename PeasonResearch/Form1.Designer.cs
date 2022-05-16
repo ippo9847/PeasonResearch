cb43b38fcee5827781e6e7e341727460b1945eb5
ippo9847 <ippo.wshh47@akane.waseda.jp>
2022-05-17 00:18:38 +0900

    プロジェクト ファイルを追加します。

---
+++ PeasonResearch/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace PeasonResearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "名前検索\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(526, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(201, 324);
+            this.listBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "一致",
+            "前方一致",
+            "含む"});
+            this.comboBox1.Location = new System.Drawing.Point(201, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "年齢検索";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "以上",
+            "等しい",
+            "以下"});
+            this.comboBox2.Location = new System.Drawing.Point(201, 187);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 27);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(31, 304);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(152, 27);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "以上",
+            "等しい",
+            "以下"});
+            this.comboBox3.Location = new System.Drawing.Point(201, 304);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(368, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "身長検索";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PeasonResearch/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PeasonResearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        public Person(string Name, int Age, int Height)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Height = Height;
+        }
+        */
+
+        class Person
+        {
+            public string Name;
+            public int Age;
+            public double Height;
+
+            public Person(string name, int age, int height)//よくわからん
+            {
+                Name = name;
+                Age = age;
+                Height = height;
+            }
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Person> a = new List<Person>();
+            listBox1.Items.Clear();
+            ////
+
+            using (StreamReader sr = new StreamReader("data.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    string[] vs = s.Split(',');
+                    String Name = vs[0];
+                    int age = int.Parse(vs[1]);
+                    int height = int.Parse(vs[2]);
+                    a.Add(new Person(Name, age, height));//
+                }            
+            }
+
+            String text1 = textBox1.Text;
+            if (comboBox1.Text == "一致")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text1 == a[i].Name) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox1.Text == "前方一致")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[i].Name.StartsWith(text1)) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox1.Text == "含む")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[i].Name.Contains(text1)) listBox1.Items.Add(a[i].Name);
+                }
+            }
+
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)//年齢
+        {
+            List<Person> a = new List<Person>();
+            listBox1.Items.Clear();
+            ////
+
+            using (StreamReader sr = new StreamReader("data.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    string[] vs = s.Split(',');
+                    String Name = vs[0];
+                    int age = int.Parse(vs[1]);
+                    int height = int.Parse(vs[2]);
+                    a.Add(new Person(Name, age, height));//
+                }
+            }
+
+            int text2 = int.Parse(textBox2.Text);
+            if (comboBox2.Text == "以上")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text2 <= a[i].Age) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox2.Text == "等しい")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text2 == a[i].Age) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox2.Text == "以下")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text2 >= a[i].Age) listBox1.Items.Add(a[i].Name);
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//身長
+        {
+            List<Person> a = new List<Person>();
+            listBox1.Items.Clear();
+            ////
+
+            using (StreamReader sr = new StreamReader("data.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    string[] vs = s.Split(',');
+                    String Name = vs[0];
+                    int age = int.Parse(vs[1]);
+                    int height = int.Parse(vs[2]);
+                    a.Add(new Person(Name, age, height));//
+                }
+            }
+
+            int text3 = int.Parse(textBox3.Text);
+            if (comboBox3.Text == "以上")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text3 <= a[i].Height) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox3.Text == "等しい")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text3 == a[i].Height) listBox1.Items.Add(a[i].Name);
+                }
+            }
+            else if (comboBox3.Text == "以下")
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (text3 >= a[i].Height) listBox1.Items.Add(a[i].Name);
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
